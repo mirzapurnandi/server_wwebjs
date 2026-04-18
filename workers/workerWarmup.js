17a1f4a3bdab09b21e3fb2a1348183bd28748c8c
mirzapurnandi <mirza.purnandi@gmail.com>
2026-04-18 12:00:37 +0700

    FixBugs 057

--- workers/workerWarmup.js
+++ workers/workerWarmup.js
@@ -56,7 +56,7 @@ const worker = new Worker(
             host: "localhost",
             port: 6379,
         },
-        concurrency: 5, // Izinkan 5 balasan berjalan bersamaan
+        concurrency: 10, // Izinkan 5 balasan berjalan bersamaan
     },
 );
 
